e56ec80c9780e5b41668a095f87e09978200374b
Keaton Nordby <keatonnordby4@gmail.com>
2026-05-30 08:13:23 -0500

    implemented planner agent

--- app.py
+++ app.py
@@ -36,3 +36,28 @@ def write_text_file(filename: str, content: str):
     filepath = os.path.join(LOG_DIR, filename)
     with open(filepath, "w", encoding="utf-8") as f:
         f.write(content)
+        
+        
+
+"""
+by design - the state is essentially a dictionary that holds all info 
+flowing between agents. These agents will read from the state and send it 
+to next agent in workflow
+"""        
+def planner_agent(state):
+    logger.info("Planner agent started..")
+    logger.info(f"User query: {state['user_query']}")
+    prompt = f"""
+    You are a planning agent in a multi agent AI system.
+    
+    Your job is to read the user's query and create a short plan for how the worker agent should answer.
+    
+    User query: {state['user_query']}
+    """
+    response = llm.invoke(prompt)
+    plan = response.content if hasattr(response, "content") else str(response)
+    """ Here we update the state to get ready to hand it off to next agent """
+    state['plan'] = plan 
+    write_text_file("planner_output.txt", plan)
+    logger.info(f"Planner output:{plan}")
+    return state
